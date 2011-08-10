9447ca6e00f9f0c647f453f8fd0ce028ea9de3ea
rudylattae <rudylattae@gmail.com>
2011-08-10 01:39:01 -0600

    Improve build/package/install tools.

--- install.cmd
+++ install.cmd
@@ -3,17 +3,37 @@ setlocal
 :: Simple development and build automation
 
 set gadget_name=TimeSense
+set version=dev
+set package_name=TimeSense-%version%
 set source_dir=%~p0%gadget_name%.gadget
-set install_dir=%LOCALAPPDATA%\Microsoft\Windows Sidebar\Gadgets\%gadget_name%.gadget
+set build_dir=%~p0build\%package_name%.gadget
+set install_dir=%LOCALAPPDATA%\Microsoft\Windows Sidebar\Gadgets\%package_name%.gadget
 
 echo.
-echo Installing %gadget_name%.gadget
-echo =====================================
+echo Building...
+echo -------------------------------
+echo.
+echo in: %source_dir%
+echo out: %build_dir%
+echo.
+
+if not exist "%build_dir%" (
+    echo "Creating build directory for %package_name%"
+    mkdir "%build_dir%"
+)
+
+xcopy /E "%source_dir%\*.*" "%build_dir%"
+endlocal
+goto :eof
+
+echo.
+echo Installing %package_name%.gadget
+echo -------------------------------
 echo.
 
 :: clean-install the gadget
 if not exist "%install_dir%" (
-    echo "Creating install directory for %gadget_name%"
+    echo "Creating install directory for %package_name%"
     mkdir "%install_dir%"
 )
 
