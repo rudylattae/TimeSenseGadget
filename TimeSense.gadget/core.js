49c1724b8223567ea1491d7e869f436dc4f99309
rudylattae <rudylattae@gmail.com>
2011-08-25 18:42:26 -0600

    Settings are now persisted. Timer is initialized with user defined options.

--- TimeSense.gadget/core.js
+++ TimeSense.gadget/core.js
@@ -4,7 +4,10 @@
  */
 function Configurator() {
     var defaults =  {
-        'what': 'Some default thing'
+        'event': 'New Year 2012!',
+        'focus': 'Jan 1 2012',
+        'reference': _date(Date.now()).format('MMM D YYYY hh:mm:ss a'),
+        'toggleDateEvent': 'event'
     };
     this.values = defaults;
 }
@@ -13,14 +16,32 @@ Configurator.prototype = {
     load: function() {
         if (!System.Gadget.Settings.read('customized')) return;
         
-        if (System.Gadget.Settings.readString('what')) {
-            this.values['what'] = System.Gadget.Settings.readString('what');
+        if (System.Gadget.Settings.readString('event')) {
+            this.values['event'] = System.Gadget.Settings.readString('event');
+        }
+        if (System.Gadget.Settings.readString('focus')) {
+            this.values['focus'] = System.Gadget.Settings.readString('focus');
+        }
+        if (System.Gadget.Settings.readString('reference')) {
+            this.values['reference'] = System.Gadget.Settings.readString('reference');
+        }
+        if (System.Gadget.Settings.readString('toggleDateEvent')) {
+            this.values['toggleDateEvent'] = System.Gadget.Settings.readString('toggleDateEvent');
         }
     },
     
     save: function() {
-        if (typeof this.values['what'] !== 'undefined') {
-            System.Gadget.Settings.writeString('what', this.values['what']);
+        if (typeof this.values['event'] !== 'undefined') {
+            System.Gadget.Settings.writeString('event', this.values['event']);
+        }
+        if (typeof this.values['focus'] !== 'undefined') {
+            System.Gadget.Settings.writeString('focus', this.values['focus']);
+        }
+        if (typeof this.values['reference'] !== 'undefined') {
+            System.Gadget.Settings.writeString('reference', this.values['reference']);
+        }
+        if (typeof this.values['toggleDateEvent'] !== 'undefined') {
+            System.Gadget.Settings.writeString('toggleDateEvent', this.values['toggleDateEvent']);
         }
         
         System.Gadget.Settings.write('customized', true);
